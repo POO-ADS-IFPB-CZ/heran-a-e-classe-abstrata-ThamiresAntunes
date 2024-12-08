0931cc41ba131a07ef3d08464f2d8941a7339268
ThamiresAntunes <antunesthamires8@gmail.com>
2024-12-08 00:15:57 -0300

    finalizando o menu de controle

--- AtividadeHeranca/src/view/Main.java
+++ AtividadeHeranca/src/view/Main.java
@@ -13,9 +13,7 @@ public class Main {
         Conta conta = null;
         Scanner scanner = new Scanner(System.in);
         int opcao, tipoConta;
-
-
-
+        double valor;
         do{
             System.out.println("--Menu Conta Bancaria--");
             System.out.println("1 - Abrir uma conta | 2 - Depositar | 3 - Sacar | 4 - Ver Saldo | 0 - Sair |  ");
@@ -31,16 +29,24 @@ public class Main {
                         case 2: conta = new ContaEspecial(3, "Maria", 100); break;
                         case 3: conta = new ContaPoupanca(5, "Pedro");
                     }
+                case 2:
+                    System.out.println("Digite o valor do deposito ");
+                    valor = scanner.nextDouble();
+                    conta.depositar(valor); break;
+                case 3:
+                    System.out.println("Digite o valor do saque ");
+                    valor = scanner.nextDouble();
+                    conta.sacar(valor); break;
+                case 4:
+                    System.out.println("Saldo Atual da Conta: R$" + conta.getSaldo()); break;
+                case 0: break;
+                default:
+                    System.out.println("Opção inválida...");
             }
 
         }
         while(opcao != 0);
 
-        switch (opcao){
-            case 1:
-                conta = new Conta(1, "José");
-
-        }
 
     }
 }
